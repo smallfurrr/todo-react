aeda384d98b294e673301be40e1e61ddb13dfa6d
EliseChia <swedesurrender@gmail.com>
2019-08-30 09:15:27 +0800

    done until validity~

--- script.jsx
+++ script.jsx
@@ -15,7 +15,7 @@ class List extends React.Component {
             word: "",
             list: [],
             inputClass:"",
-            status:""
+            valid: true
         }
     }
 
@@ -36,7 +36,7 @@ class List extends React.Component {
         if (newEntry.length < 2 || newEntry.length > 200) {
             this.setState({
                 inputClass: "warning",
-                status: "Entry must be between 2 and 5 characters!"
+                valid: false
             }, () => {
                 console.log(this.state.inputClass);
             });
@@ -44,7 +44,9 @@ class List extends React.Component {
         } else {
             //add entry to array
             this.setState({
-                list: [...this.state.list, newEntry]
+                list: [...this.state.list, newEntry],
+                valid: true
+                //change the this.state.inputclass to the non-warning class
             }, () => {
                 console.log(this.state.list)
             });
@@ -54,14 +56,17 @@ class List extends React.Component {
   render() {
 
       const taskList = this.state.list.map((task, taskIndex) => {
-
         return(
             <Task key={taskIndex} item={task}/>
         );
       })
 
+      let header = <h2>To-do List</h2>;
+      if (!this.state.valid) header = <h2>Entry must be between 2 and 5 characters!</h2>;
+
       return (
         <div className="list">
+          <div className="header">{header}</div>
           <input className={this.state.inputClass} onChange={()=>{this.changeHandler()}} value={this.state.word}/>
           <button onClick={()=>{this.addItem()}}>add item</button>
           <ul>
